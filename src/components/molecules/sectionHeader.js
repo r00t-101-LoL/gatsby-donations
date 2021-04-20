dc649e8af5ccd08c7619fe0a356db9f412d61514
r00t-101-LoL <adhd101js@gmail.com>
2021-04-20 12:39:19 +0000

    sectionHeader.js: updated Header

--- src/components/molecules/sectionHeader.js
+++ src/components/molecules/sectionHeader.js
@@ -24,7 +24,7 @@ const SectionHeader = () => {
         <Img fluid={data.file.childImageSharp.fluid} />
       </Image>
       <Content>
-        <h1>Do More </h1>
+        <h1>Do More To</h1>
         <p>
           Set up beautiful donations forms with a flexible back-end. Built on
           top of Stripe. No coding, merchant account, or hosting required.
