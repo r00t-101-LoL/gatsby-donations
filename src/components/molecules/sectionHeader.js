48e7dbaf1645a137dd5124b950688f8ffbc1c88a
Stackbit Code Editor <projects@stackbit.com>
2021-04-20 12:55:41 +0000

    sectionHeader.js: changed

--- src/components/molecules/sectionHeader.js
+++ src/components/molecules/sectionHeader.js
@@ -9,7 +9,7 @@ import { media } from "@utils/media"
 const SectionHeader = () => {
   const data = useStaticQuery(graphql`
     query HeaderQuery {
-      file(relativePath: { eq: "top.png" }) {
+      file(relativePath: { eq: "headerImage.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 800) {
             ...GatsbyImageSharpFluid_withWebp_tracedSVG
